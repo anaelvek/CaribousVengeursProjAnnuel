29c41cd58c83cbed99ac23617844f489ca8a95fe
WpFeltac <67151803+WpFeltac@users.noreply.github.com>
2021-03-22 16:39:20 +0100

    Index.html --> .php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Les Cariboux Vengeurs</title>
+</head>
+<body>
+	<h1><center>Les Cariboux Vengeurs</center></h1>
+		<br><br><br><br><br>
+		
+		
+		<div><a href="https://je-code.com/esgi1/adavid28/">Achille DAVID</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/atruffier/CV/">Aurelien TRUFFIER</a></div>
+		
+		<div><a href="">Anaël VEKEMANS [En cours de rédaction]</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/bkhemissi1/">Bahya KHEMISSI</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/eelkihel/">Elias EL KIHEL</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/hberleur/cvHtml/">Hugo BERLEUR</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/hdoom/site/">Hugo DOOM</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/jderache/">Julien DERACHE</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/mboureau2/page/">Matthys BOUREAU</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/qgransart/cours/">Quentin GRANSART</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/tdusautoir/cvphp/">Thibaut DUSAUTOIR</a></div>
+		
+		<div><a href="https://je-code.com/esgi1/tvinchent/">Thibault VINCHENT</a></div>
+
+</body>
+</html>
