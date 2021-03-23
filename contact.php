351de20020ea263ed2b7a7d1d1e9d0939aa36e78
Matthys-Boureau <73697329+Matthys-Boureau@users.noreply.github.com>
2021-03-23 12:53:40 +0100

    Menu + Page Admin en HTML

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact - Les Caribous Vengeurs</title>
+</head>
+<body>
+<h1><center>Page de Contact</center></h1>
+<br><br><br><br><br>
+<button type = "button"><a href="index.php">Accueil</a></button> 
+<button type = "button"><a href="contact.php">Contact</a></button>      
+<button type = "button"><a href="admin.php">Administration</a></button>
+<br>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,35 +5,71 @@
 </head>
 	<body>
 		
-		<h1>
-			<center>Les Caribous Vengeurs</center>
-		</h1>
-			<br><br>
-			<div><a href="https://je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></div>		
+<h1>
+	<center>Les Caribous Vengeurs</center>
+</h1>
+	<br><br><br><br><br>
+		<!--<div><a href="https://je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></div> -->
+
+		<button type = "button"><a href="index.php">Accueil</a></button>
+
+		<button type = "button"><a href="contact.php">Contact</a></button>
+
+	<br><br><br><br><br>			
 			
-			<br><br><br><br><br>			
+			<div>
+				<a href="https://je-code.com/esgi1/adavid28/">Achille DAV.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/adavid28/">Achille DAV.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/atruffier/CV/">Aurelien TRU.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/atruffier/CV/">Aurelien TRU.</a></div>
+			<div>
+				<a href="">Anaël VEK. [En cours de rédaction]</a>
+			</div>
 			
-			<div><a href="">Anaël VEK. [En cours de rédaction]</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/bkhemissi1/">Bahya KHE.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/bkhemissi1/">Bahya KHE.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/eelkihel/">Elias ELK.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/eelkihel/">Elias ELK.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/hberleur/cvHtml/">Hugo BER.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/hberleur/cvHtml/">Hugo BER.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/hdoom/site/">Hugo DOO.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/hdoom/site/">Hugo DOO.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/jderache/">Julien DER..
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/jderache/">Julien DER..</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/mboureau2/page/">Matthys BOU.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/mboureau2/page/">Matthys BOU.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/qgransart/cours/">Quentin GRA.
+				</a>
+			</div>
 			
-			<div><a href="https://je-code.com/esgi1/qgransart/cours/">Quentin GRA.</a></div>
-			
-			<div><a href="https://je-code.com/esgi1/tdusautoir/cvphp/">Thibaut DUS.</a></div>
+			<div>
+				<a href="https://je-code.com/esgi1/tdusautoir/cvphp/">Thibaut DUS.
+				</a>
+			</div>
 		
 	</body>
 </html>
