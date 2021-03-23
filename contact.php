f0b204bf72c9d80bd1e5a26d67193d62217cbf9e
WpFeltac <hberleur@gmail.com>
2021-03-23 15:35:58 +0100

    Revert "Id pour les DIV / Du CSS (Background/texte) et link"
    This reverts commit 4ab7ada0f618d59ad3726ee6306917a4bcbffca6.

--- contact.php
+++ contact.php
@@ -2,25 +2,19 @@
 <html>
 <head>
 	<title>Contact - Les Caribous Vengeurs</title>
-	<link rel="stylesheet" href="style.css">
 </head>
 		<body>
 
 		<h1><center>Page de Contact</center></h1>
 
 		<br><br><br><br><br>
-<!--		ONLINE
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php">Accueil</a></button>
 
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php">Contact</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php">Accueil</a></button>
 
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></button>
--->		
-				<button type = "button" id="bouton"><a href="index.php">Accueil</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php">Contact</a></button>
 
-		<button type = "button" id="bouton"><a href="contact.php">Contact</a></button>
-
-		<button type = "button" id="bouton"><a href="admin.php">Administration</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></button>
+		
 		<br>
 
 	</body>
--- index.php
+++ index.php
@@ -9,76 +9,69 @@
 <h1>
 	<center>Les Caribous Vengeurs</center>
 </h1>
-	<br><br><br><br>
-<!--
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php">Accueil</a></button>
+	<br><br><br><br><br>
 
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php">Contact</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php">Accueil</a></button>
 
-		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></button>
--->
-		<button type = "button" id="bouton"><a href="index.php">Accueil</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php">Contact</a></button>
 
-		<button type = "button" id="bouton"><a href="contact.php">Contact</a></button>
+		<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></button>
 
-		<button type = "button" id="bouton"><a href="admin.php">Administration</a></button>
-	<br><br><br>			
-			<center>
-			<div id="liste">
-				
+	<br><br><br><br><br>			
+			
+			<div>
 				<a href="https://je-code.com/esgi1/adavid28/">Achille DAV.
 				</a>
-			<br><br>
+			</div>
 			
-			
+			<div>
 				<a href="https://je-code.com/esgi1/atruffier/CV/">Aurelien TRU.
 				</a>
-			<br><br>
+			</div>
 			
-			
+			<div>
 				<a href="https://www.je-code.com/esgi1/avekemans/">Anaël VEK.</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/bkhemissi1/cv/">Bahya KHE.
 				</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/eelkihel/">Elias ELK.
 				</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/hberleur/cvHtml/">Hugo BER.
 				</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/hdoom/site/">Hugo DOO.
 				</a>
+			</div>
 			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/jderache/">Julien DER.
+			<div>
+				<a href="https://je-code.com/esgi1/jderache/">Julien DER..
 				</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/mboureau2/page/">Matthys BOU.
 				</a>
+			</div>
 			
-			<br><br>
-			
+			<div>
 				<a href="https://je-code.com/esgi1/qgransart/cours/">Quentin GRA.
 				</a>
-		
-			<br><br>
+			</div>
 			
+			<div>
 				<a href="https://je-code.com/esgi1/tdusautoir/cvphp/">Thibaut DUS.
 				</a>
-			
 			</div>
-		</center>
+		
 	</body>
 </html>
