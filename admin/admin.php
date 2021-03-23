4c4e6419d9b8060656743c822a1e6f458340f4a5
WpFeltac <67151803+WpFeltac@users.noreply.github.com>
2021-03-23 14:52:44 +0100

    update css

---
+++ admin/admin.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administration - Les Caribous Vengeurs</title>
+</head>
+
+<body>
+ <h1><center>PAGE ADMINISTRATION</center></h1>
+
+	<br><br><br><br><br>
+
+	<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php">Accueil</a></button>
+
+	<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php">Contact</a></button>
+
+	<button type = "button"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php">Administration</a></button>
+
+ 	<br><br><br><br><br>
+ 	
+ <p>
+ 	<center>
+ 		<form action="#" method="post" id="form" name="form" onsubmit="envoiForm()">
+			Achille DAV.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Aurelien TRU.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Anaël VEK.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Bahya KHE.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Elias ELK.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Hugo BER.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Hugo DOO.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Julien DER.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Matthys BOU.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Quentin GRA.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Thibaut DUS.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			<button type="button" value="Sauvegarder">Sauvegarder</button>
+		</form>
+	</center>
+</p>
+
+
+
+
+</body>
+
+
+</html>
