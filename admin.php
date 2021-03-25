6f04c27050fa47a9b8eca8faeb2d0e66097c3a16
Matthys-Boureau <73697329+Matthys-Boureau@users.noreply.github.com>
2021-03-25 14:51:47 +0100

    Ajout HTML/CSS (Police d'écriture, modification bouton et couleurs)
    Ca taff dur

---
+++ admin.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administration - Les Caribous Vengeurs</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+ <h1><center>Administration</center></h1>
+	<br><br><br><br>
+
+ <!--
+		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php"><b>Accueil</b></a></button>
+
+		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/contact.php"><b>Contact</b></a></button>
+
+		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php"><b>Administration</b></a></button>
+-->
+		<button type = "button" id="bouton"><a href="index.php"><b>Accueil</b></a></button>
+
+		<button type = "button" id="bouton"><a href="contact.php"><b>Contact</b></a></button>
+
+		<button type = "button" id="bouton"><a href="admin.php"><b>Administration</b></a></button>
+ 	<br><br><br>
+ <p>
+ 	<center>
+ 		<form action="#" method="post" id="form" name="form" onsubmit="envoiForm()">
+ 			<b>
+ 			<div id="liste">
+			Achille DAV.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Aurelien TRU.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Anaël VEK.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Bahya KHE.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Elias ELK.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Hugo BER.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Hugo DOO.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Julien DER.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Matthys BOU.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Quentin GRA.
+			<input type="checkbox" name="valid" tabindex="2">
+				<br><br>
+			Thibaut DUS.
+			<input type="checkbox" name="valid" tabindex="2">
+			</b>	<br><br>
+			</div>
+			<button type="button" id="bouton" value="Sauvegarder"><b><a>Sauvegarder</a></b></button>
+		</form>
+	</center>
+</p>
+
+
+
+
+</body>
+
+
+</html>
